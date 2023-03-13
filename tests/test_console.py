fa4a30f51b310cd305c5506b830ceda2ecddeba0
NaijasonEzeiru <67031753+NaijasonEzeiru@users.noreply.github.com>
2023-03-13 04:53:13 +0100

    seventh

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,133 +1,144 @@
 #!/usr/bin/python3
-"""Test Console Module"""
+""" test for console"""
 
-from console import HBNBCommand
-from models.engine.file_storage import FileStorage
+import sys
+import models
 import unittest
 from io import StringIO
-import sys
-import datetime
-from unittest.mock import patch
-import re
-import os
+from console import HBNBCommand
+from unittest.mock import create_autospec
 
+class test_console(unittest.TestCase):
+    ''' Test the console module'''
+    def setUp(self):
+        '''setup for'''
+        self.backup = sys.stdout
+        self.capt_out = StringIO()
+        sys.stdout = self.capt_out
 
-class Test_CommanConsole(unittest.TestCase):
-    """Test Console Module (console.py)"""
+    def tearDown(self):
+        ''''''
+        sys.stdout = self.backup
 
-    def do_quit(self, line):
-        """Quit command to exit the HBNB console"""
-        print("Thank you for using The Console")
-        return True
+    def create(self):
+        ''' create an instance of the HBNBCommand class'''
+        return HBNBCommand()
 
-    def do_EOF(self, line):
-        """Quit command to exit the program at end of file"""
-        print()
-        return True
+    def test_quit(self):
+        ''' Test quit exists'''
+        console = self.create()
+        self.assertTrue(console.onecmd("quit"))
 
-    def emptyline(self):
-        """Ingnore empty line"""
-        pass
+    def test_EOF(self):
+        ''' Test EOF exists'''
+        console = self.create()
+        self.assertTrue(console.onecmd("EOF"))
 
-    def do_create(self, line):
-        """
-        Creates a new instance of BaseModel, saves it
-        (to the JSON file) and prints the id.
-        """
-        if not line:
-            print("** class name missing **")
-        elif line in class_check:
-            _input = line.split()
-            new_obj = class_check[_input[0]]()
-            new_obj.save()
-            storage.reload()
-            print(new_obj.id)
-        else:
-            print("** class doesn't exist **")
+    def test_all(self):
+        ''' Test all exists'''
+        console = self.create()
+        console.onecmd("all")
+        self.assertTrue(isinstance(self.capt_out.getvalue(), str))
 
-    def do_show(self, line):
-        """
-        Prints the string representation of an instance
-        based on the class name and id
-        """
-        if line == "" or line is None:
-            print("** class name missing **")
-        else:
-            _input = line.split(' ')
-            if _input[0] not in class_check:
-                print("** class doesn't exist **")
-            elif len(_input) < 2:
-                print("** instance id missing **")
-            else:
-                key = "{}.{}".format(_input[0], _input[1])
-                if key not in storage.all():
-                    print("** no instance found **")
-                else:
-                    print(storage.all()[key])
+    def test_show(self):
+        '''
+        Testing that show exists
+        '''
+        console = self.create()
+        console.onecmd("create User")
+        user_id = self.capt_out.getvalue()
+        sys.stdout = self.backup
+        self.capt_out.close()
+        self.capt_out = StringIO()
+        sys.stdout = self.capt_out
+        console.onecmd("show User " + user_id)
+        x = (self.capt_out.getvalue())
+        sys.stdout = self.backup
+        self.assertTrue(str is type(x))
 
-    def do_destroy(self, line):
-        """
-        Deletes an instance based on the class name and id
-        (save the change into the JSON file)
-        """
-        if line == "" or line is None:
-            print("** class name missing **")
-        else:
-            _input = line.split(' ')
-            if _input[0] not in class_check():
-                print("** class doesn't exist **")
-            elif len(_input) < 2:
-                print("** instance id missing **")
-            else:
-                key = "{}.{}".format(_input[0], _input[1])
-                if key not in storage.all():
-                    print("** no instance found **")
-                else:
-                    del storage.all()[key]
-                    storage.save()
+        def test_show_class_name(self):
+            '''
+            Testing the error messages for class name missing.
+            '''
+            console = self.create()
+            console.onecmd("create User")
+            user_id = self.capt_out.getvalue()
+            sys.stdout = self.backup
+            self.capt_out.close()
+            self.capt_out = StringIO()
+            sys.stdout = self.capt_out
+            console.onecmd("show")
+            x = (self.capt_out.getvalue())
+            sys.stdout = self.backup
+            self.assertEqual("** class name missing **\n", x)
 
-    def do_all(self, usr_in):
-        """Prints all instances based on a class name"""
-        if name:
-            if name in class_check:
-                for key, value in (storage.all()).items():
-                    if name in key:
-                        print(value)
-            else:
-                print("** class doesn't exist **")
-        else:
-            for value in storage.all().values():
-                print(value)
+        def test_show_class_name2(self):
+            '''
+            Test show message error for id missing
+            '''
+            console = self.create()
+            console.onecmd("create User")
+            user_id = self.capt_out.getvalue()
+            sys.stdout = self.backup
+            self.capt_out.close()
+            self.capt_out = StringIO()
+            sys.stdout = self.capt_out
+            console.onecmd("show User")
+            x = (self.capt_out.getvalue())
+            sys.stdout = self.backup
+            self.assertEqual("** instance id missing **\n", x)
 
-    def do_update(self, line):
-        """
-        Updates an instance based on the class name and id
-        by adding or updating attribute(save the change into the JSON file)
-        """
-        inpu = line.split()
-        if line == "" or line is None:
-            print("** class name missing **")
-        elif inpu[0] in self.class_list:
-            if len(inpu) < 2:
-                print("**instance id missing**")
-            elif len(inpu) < 3:
-                print("**attribute name missing**")
-            elif len(inpu) < 4:
-                print("**value missing**")
-            else:
-                key = "{}.{}".format(inpu[0], inpu[1])
-                if key in storage.all():
-                    if type(inpu[3]) is dict:
-                        storage.all()[key].setattr(inpu[2], inpu[3])
-                    objs[key].__setattr__(inpu[2], inpu[3])
-                    objs[key].save()
-                else:
-                    print("**no instance found**")
-        else:
-            print("**class doesn't exist**")
+        def test_show_no_instance_found(self):
+            '''
+            Test show message error for id missing
+            '''
+            console = self.create()
+            console.onecmd("create User")
+            user_id = self.capt_out.getvalue()
+            sys.stdout = self.backup
+            self.capt_out.close()
+            self.capt_out = StringIO()
+            sys.stdout = self.capt_out
+            console.onecmd("show User " + "124356876")
+            x = (self.capt_out.getvalue())
+            sys.stdout = self.backup
+            self.assertEqual("** no instance found **\n", x)
+
+        def test_create(self):
+            '''
+            Test that create works
+            '''
+            console = self.create()
+            console.onecmd("create User")
+            self.assertTrue(isinstance(self.capt_out.getvalue(), str))
+
+        def test_class_name(self):
+            '''
+            Testing the error messages for class name missing.
+            '''
+            console = self.create()
+            console.onecmd("create")
+            x = (self.capt_out.getvalue())
+            self.assertEqual("** class name missing **\n", x)
+
+        def test_class_name_doest_exist(self):
+            '''
+            Testing the error messages for class name missing.
+            '''
+            console = self.create()
+            console.onecmd("create Binita")
+            x = (self.capt_out.getvalue())
+            self.assertEqual("** class doesn't exist **\n", x)
+
+        '''
+        def test_destroy(self):
+        console = self.create()
+        self.assertTrue(console.onecmd("destroy"))
+        def test_update(self):
+        console = self.create()
+        self.assertTrue(console.onecmd("update"))
+        '''
 
 
 if __name__ == '__main__':
-    class_check = {"Amenity", "BaseModel", "City" "Place", "Review",
-                   "State", "User"}
-    HBNBCommand().cmdloop()
+    unittest.main()
--- tests/test_models/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-''' creates a static FileStorage instance '''
-import test_engine.test_file_storage
-
-
-test_storage = TestFileStorage()
-teststorage.reload()
